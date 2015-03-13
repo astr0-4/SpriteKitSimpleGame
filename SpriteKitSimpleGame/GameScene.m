a73e4930d673e61362bded5e43c69e0fabb2a7bb
Alex Stroulger <alexstroulger@gmail.com>
2015-03-13 11:27:17 -0700

    Added Ninja, monsters, and shooting

--- SpriteKitSimpleGame/GameScene.m
+++ SpriteKitSimpleGame/GameScene.m
@@ -8,42 +8,173 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
+@interface GameScene()
+@property (nonatomic) SKSpriteNode *player;
+@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
+@end
 
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+static inline CGPoint rwAdd(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+static inline CGPoint rwSub(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint rwMult(CGPoint a, float b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline float rwLength(CGPoint a) {
+    return sqrt(a.x * a.x + a.y * a.y);
+}
+
+//Normalizes the vector
+static inline CGPoint rwNormalize(CGPoint a) {
+    float length = rwLength(a);
+    return CGPointMake(a.x/length, a.y/length);
+}
+
+@implementation GameScene
+
+-(id) initWithSize:(CGSize)size {
+    if(self = [super initWithSize:size]) {
+        //2
+        NSLog(@"Size: %@", NSStringFromCGSize(size));
+        
+        //3
+        self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+        
+        //4
+        self.player = [SKSpriteNode spriteNodeWithImageNamed: @"player"];
+        self.player.position = CGPointMake(self.player.size.width/2, self.frame.size.height/2);
+        [self addChild:self.player];
+    }
+    return self;
+}
+
+-(void)addMonster {
+    //Create Sprite
+    SKSpriteNode *monster = [SKSpriteNode spriteNodeWithImageNamed: @"monster"];
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    //Determine where to spawn the monster along the Y axis
+    int minY = monster.size.height/2;
+    int maxY = self.frame.size.height - monster.size.height/2;
+    int rangeY = maxY- minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create monster slightly off screen along the right edge, and along
+    // a random position along the Y axis as calcluated above.
+    monster.position = CGPointMake(self.frame.size.width + monster.size.width/2, actualY);
+    [self addChild:monster];
+    
+    // determine speed of monster
+    int minDuration = 1.0;
+    int maxDuration = 2.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    //create the actions
+    SKAction *actionMove = [SKAction moveTo:CGPointMake(-monster.size.width/2, actualY) duration:actualDuration];
+    SKAction *actionMoveDone = [SKAction removeFromParent];
+    [monster runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+    
+}
+
+-(void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
+    self.lastSpawnTimeInterval += timeSinceLast;
+    if (self.lastSpawnTimeInterval > 1) {
+        self.lastSpawnTimeInterval = 0;
+        [self addMonster];
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+-(void)update:(NSTimeInterval)currentTime {
+    //Handle time delta
+    //if we drop below 60fps, we still want everything to move the same distance.
+    
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    if(timeSinceLast > 1) {
+        timeSinceLast = 1.0 / 60.0;
+        self.lastUpdateTimeInterval = currentTime;
+    }
+    
+    [self updateWithTimeSinceLastUpdate: timeSinceLast];
 }
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    // 1 choose one of your touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    
+    // 2 set up initial location of projectile
+    SKSpriteNode *projectile = [SKSpriteNode spriteNodeWithImageNamed:@"projectile"];
+    projectile.position = self.player.position;
+    
+    // 3 determine offset of location of projectile
+    CGPoint offset = rwSub(location, projectile.position);
+    
+    // 4 bail out if you are shooting down or backwards
+    if(offset.x <= 0) return;
+    
+    // 5 - OK to add now that we have double checked the position
+    [self addChild:projectile];
+    
+    //6 - get the direction of where to shoot
+    CGPoint direction = rwNormalize(offset);
+    
+    //7 - make it shoot far enough to be guaranteed off screen;
+    CGPoint shootAmount = rwMult(direction, 1000);
+    
+    //8 - add the shoot amount to the current position
+    CGPoint realDest = rwAdd(shootAmount, projectile.position);
+    
+    //9 - create the actions
+    float velocity = 200.0/1.0;
+    float realMoveDuration = self.size.width / velocity;
+    SKAction *actionMove = [SKAction moveTo:realDest duration:realMoveDuration];
+    SKAction *actionMoveDone = [SKAction removeFromParent];
+    [projectile runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+    
+    
+}
+
+//-(void)didMoveToView:(SKView *)view {
+//    /* Setup your scene here */
+//    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+//    
+//    myLabel.text = @"Hello, World!";
+//    myLabel.fontSize = 65;
+//    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+//                                   CGRectGetMidY(self.frame));
+//    
+//    [self addChild:myLabel];
+//}
+//
+//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+//    /* Called when a touch begins */
+//    
+//    for (UITouch *touch in touches) {
+//        CGPoint location = [touch locationInNode:self];
+//        
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
+//    }
+//}
+//
+//-(void)update:(CFTimeInterval)currentTime {
+//    /* Called before each frame is rendered */
+//}
 
 @end
